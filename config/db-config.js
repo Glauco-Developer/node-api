adffca835ca3531e46032b9a9b8e5910827c656d
glauco.developer@gmail.com <glauco.developer@gmail.com>
2020-03-07 10:02:29 +0000

    removed config

--- config/db-config.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/node-backend')
-    .then(() => console.log('connected to mongodb'))
-    .catch(err => console.log('could not connect to mongodb', err))
-
-module.exports = mongoose;
